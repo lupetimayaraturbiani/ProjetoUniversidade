f2eae44c19abfa2d5b075920af636422e2fba69d
IsaMirandaP <pereiraisabelamira>
2025-11-04 06:05:31 -0300

    repositorio de disciplina e professor

---
+++ ProjetoUniversidade/ProjetoUniversidade/src/main/java/repository/ProfessorRepository.java
@@ -0,0 +1,8 @@
+package repository;
+
+import model.Professor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProfessorRepository extends JpaRepository<Professor, Long> {
+
+}
