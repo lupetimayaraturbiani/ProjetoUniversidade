cda52d15c8fce83852e5271eb3a5f0373c032b3e
lupetimayaraturbiani <mayaralupetiturbiani@gmail.com>
2025-11-04 05:12:16 -0300

    adicionando base do projeto com o application ja formatado

---
+++ ProjetoUniversidade/ProjetoUniversidade/src/test/java/com/example/ProjetoUniversidade/ProjetoUniversidadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProjetoUniversidade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoUniversidadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
