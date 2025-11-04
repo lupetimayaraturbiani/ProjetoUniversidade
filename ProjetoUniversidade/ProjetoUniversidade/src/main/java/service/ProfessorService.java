534d453cf6856307fd48aae7a7d241ff4860c87d
lupetimayaraturbiani <mayaralupetiturbiani@gmail.com>
2025-11-04 06:17:00 -0300

    adicionando service do professor

---
+++ ProjetoUniversidade/ProjetoUniversidade/src/main/java/service/ProfessorService.java
@@ -0,0 +1,41 @@
+package service;
+
+import model.Professor;
+import org.springframework.stereotype.Service;
+import repository.ProfessorRepository;
+
+import java.util.List;
+
+@Service
+public class ProfessorService {
+    private final ProfessorRepository professorRepository;
+
+    public ProfessorService(ProfessorRepository professorRepository) {
+        this.professorRepository = professorRepository;
+    }
+
+    public List<Professor> listarTodos(){
+        return professorRepository.findAll();
+    }
+
+    public Professor buscarPorId(Long id) {
+        return professorRepository.findById(id).orElse(null);
+    }
+
+    public Professor cadastrar(Professor professor) {
+        return professorRepository.save(professor);
+    }
+
+    public Professor atualizar(Long id, Professor professor) {
+        Professor atual = professorRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Professor não encontrado(a)"));
+
+        atual.setNome(professor.getNome());
+        atual.setEspecialidade(professor.getEspecialidade());
+        return professorRepository.save(atual);
+    }
+
+    public void deletar(Long id){
+        professorRepository.deleteById(id);
+    }
+}
