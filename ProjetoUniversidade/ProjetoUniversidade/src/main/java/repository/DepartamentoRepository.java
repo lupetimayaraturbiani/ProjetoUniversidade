3aa3a0e14264ab3ea3248cb4dd1d7fe331a17ec6
JulianaR07 <juliana02oliver@gmail.com>
2025-11-04 05:58:38 -0300

    adicionando repositórios de curso e departamento

---
+++ ProjetoUniversidade/ProjetoUniversidade/src/main/java/repository/DepartamentoRepository.java
@@ -0,0 +1,8 @@
+package repository;
+
+import model.Departamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DepartamentoRepository extends JpaRepository<Departamento, Long> {
+
+}
